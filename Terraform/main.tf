a894eb8290a19f44b3825d7eef7c1eacf2a54497
rbikai <rabih.bikai@teolia.fr>
2023-09-14 09:43:52 +0200

    Fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,7 +2,7 @@ resource "random_pet" "rg_name" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
+  name     = "inviting-shrew"
   location = var.resource_group_location
 }
 
@@ -31,4 +31,23 @@ resource "azurerm_container_group" "container" {
       port     = var.port
     }
   }
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "spxstg"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
 }
