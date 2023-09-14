d4089b09108d5189dec5ff827d51cd9e4a438d82
rbikai <rabih.bikai@teolia.fr>
2023-09-14 16:24:28 +0200

    Fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,42 +13,42 @@ resource "random_string" "container_name" {
   special = false
 }
 
-resource "azurerm_container_group" "container" {
-  name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "Public"
-  os_type             = "Linux"
-  restart_policy      = var.restart_policy
+# # resource "azurerm_container_group" "container" {
+# #   name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
+# #   location            = azurerm_resource_group.rg.location
+# #   resource_group_name = azurerm_resource_group.rg.name
+# #   ip_address_type     = "Public"
+# #   os_type             = "Linux"
+# #   restart_policy      = var.restart_policy
 
-  container {
-    name   = "${var.container_name_prefix}-${random_string.container_name.result}"
-    image  = var.image
-    cpu    = var.cpu_cores
-    memory = var.memory_in_gb
+# #   container {
+# #     name   = "${var.container_name_prefix}-${random_string.container_name.result}"
+# #     image  = var.image
+# #     cpu    = var.cpu_cores
+# #     memory = var.memory_in_gb
 
-    ports {
-      port     = var.port
-    }
-  }
-}
+# #     ports {
+# #       port     = var.port
+# #     }
+# #   }
+# # }
 
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "sprixystg"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_nested_items_to_be_public = false
-  min_tls_version = "TLS1_2"
+# resource "azurerm_storage_account" "tfstate" {
+#   name                     = "sprixystg"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   allow_nested_items_to_be_public = false
+#   min_tls_version = "TLS1_2"
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "tfstate" {
+#   name                  = "tfstate"
+#   storage_account_name  = azurerm_storage_account.tfstate.name
+#   container_access_type = "private"
+# }
--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -10,13 +10,6 @@ terraform {
       version = "~>3.0"
     }
   }
-  # backend "remote" {
-  #   hostname = "app.terraform.io"
-  #   organization = "my_terraform_lab"
-  #   workspaces {
-  #     name = "TerraformCI"
-  #   }
-  # }
   backend "azurerm" {
     resource_group_name  = "inviting-shrew"
     storage_account_name = "sprixystg"
