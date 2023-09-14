c6b9697834082cdbbfe6e96a9cde633ab42e71ed
rbikai <rabih.bikai@teolia.fr>
2023-09-14 15:56:49 +0200

    Fix

--- Terraform/login.sh
+++ Terraform/login.sh
@@ -5,4 +5,5 @@ export ARM_TENANT_ID=${{ secrets.AZ_TENANT }}
 export AZURE_CLIENT_ID=${{ secrets.AZ_USER }}
 export AZURE_CLIENT_SECRET=${{ secrets.AZ_PASSWORD }}
 export AZURE_SUBSCRIPTION_ID=${{ secrets.AZ_SUBSCRIPTION }}
-export AZURE_TENANT_ID=${{ secrets.AZ_TENANT }}Terraform Destroy
+export AZURE_TENANT_ID=${{ secrets.AZ_TENANT }}
+echo "Logged in"
