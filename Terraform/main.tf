c2c7dea355ff13b38340e89ce192c4ac49b0dab2
rbikai <rabih.bikai@teolia.fr>
2023-09-14 10:31:21 +0200

    Fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_container_group" "container" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "spxstg"
+  name                     = "sprixystg"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
