829d359e42fe4f30f00a3cfbf2b696ab4fce98bb
rbikai <rabih.bikai@teolia.fr>
2023-09-14 15:51:26 +0200

    Fix

---
+++ Terraform/login.sh
@@ -0,0 +1,8 @@
+export ARM_CLIENT_ID=${{ secrets.AZ_USER }}
+export ARM_CLIENT_SECRET=${{ secrets.AZ_PASSWORD }}
+export ARM_SUBSCRIPTION_ID=${{ secrets.AZ_SUBSCRIPTION }}
+export ARM_TENANT_ID=${{ secrets.AZ_TENANT }}
+export AZURE_CLIENT_ID=${{ secrets.AZ_USER }}
+export AZURE_CLIENT_SECRET=${{ secrets.AZ_PASSWORD }}
+export AZURE_SUBSCRIPTION_ID=${{ secrets.AZ_SUBSCRIPTION }}
+export AZURE_TENANT_ID=${{ secrets.AZ_TENANT }}Terraform Destroy
