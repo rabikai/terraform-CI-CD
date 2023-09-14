cd894caa67a142805d151f46514d1ee2ed8016b5
rbikai <rabih.bikai@teolia.fr>
2023-09-14 10:37:38 +0200

    Fix

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -10,12 +10,19 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "inviting-shrew"
-    storage_account_name = "sprixystg"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "my_terraform_lab"
+    workspaces {
+      name = "TerraformCI"
+    }
   }
+  # backend "azurerm" {
+  #   resource_group_name  = "inviting-shrew"
+  #   storage_account_name = "sprixystg"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
 }
 provider "azurerm" {
   features {}
